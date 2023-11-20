26f5c80ea6760e7368d19f1cf62475cbc9b6c0d9
boris <borissiobko@gmail.com>
2023-11-20 15:15:00 +0300

    string -> bytes, optimization maybe

--- contains.go
+++ contains.go
@@ -1,15 +1,15 @@
 package testmod
 
 import (
+	"bytes"
 	"os"
-	"strings"
 )
 
 func Contains(filePath, substring string) (bool, error) {
-	fileContent, err := os.ReadFile(filePath)
+	content, err := os.ReadFile(filePath)
 	if err != nil {
 		return false, err
 	}
 
-	return strings.Contains(string(fileContent), substring), nil
+	return bytes.Contains(content, []byte(substring)), nil
 }
